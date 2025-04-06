c54d5c6ba1a010ae9833f2bec0dff865c644997f
kingr <kingrushii20@gmail.com>
2025-04-07 01:54:16 +0530

    Initial and Final Commit

---
+++ src/test/java/MavenForJenkins/UITest.java
@@ -0,0 +1,26 @@
+package MavenForJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UITest {
+
+	@Test
+	public void StartBrowser() {
+		// WebDriverManager.chromedriver().setup();
+		String path = "C:\\Users\\kingr\\eclipse-workspace\\MavenProject\\Drivers";
+		System.setProperty("webdriver.chrome.driver", path + "/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.orangehrm.com/");
+		String ActualTitle = driver.getTitle();
+		String ExpectedTitle = "Human Resources Management Software | OrangeHRM";
+		Assert.assertEquals(ExpectedTitle, ActualTitle);
+		// Assert.assertTrue(driver.getTitle().contains("Human Resources Management
+		// Software | OrangeHRM"), "Title does not match");
+		driver.quit();
+	}
+
+}
